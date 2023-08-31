47b688b1d46488555ac0ff6b0dae306ebb7648fc
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-08-31 01:10:53 -0400

    Add react-icons, start shopping list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaPlus } from "react-icons/fa6";
+//import { FaMinus } from "react-icons/fa6";
+import { FaCheck } from "react-icons/fa6";
+import { FaX } from "react-icons/fa6";
+
+
+
+const products = [
+  {title: 'Cabbage', id:1},
+  {title: 'Apples', id:2},
+  {title: 'Milk', id:3},
+  {title: 'Eggs', id:4},
+  {title: 'Steak', id:5},
+  {title: 'Butter', id:6},
+  {title: 'Rice', id:7},
+];
 
 function App() {
+  const listItems = products.map(product => 
+    <li key={product.id}
+      style={{color:product.id%2===0?'magenta':'darkgreen'}}>
+      {product.title} <span><FaCheck /><FaX /></span>
+    </li>
+  );
+// TO DO: ADD BUTTON AT TOP OF LIST, EACH LI HAS
+// X ON FAR RIGHT FOR DELETE ITEM, COMPLETED NEXT TO IT
+// HOW TO USE ICONS IN REACT
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="shopping-list">
+      <div className="container">
+      <h1>My Shopping List</h1>
+      <div><FaPlus />  Add item to List</div>
+        <ul>{listItems}</ul>
+      </div>
+      
     </div>
   );
 }
